33a07b1cabb8a0f20fa166877859b3ea70fd4258
HoangHieu <dinhhoanghieu.hanbiro@gmail.com>
2023-11-30 16:52:23 +0700

    Learn create ReactJs UI with MUI

--- src/components/about-us/AboutUs.jsx
+++ src/components/about-us/AboutUs.jsx
@@ -6,7 +6,78 @@ import SecondaryButton from "../secondary-button/SecondaryButton";
 import aboutUsImg from "../../assets/main/about-us-img.svg";
 
 const AboutUs = () => {
-  return <Typography>About Us</Typography>;
+  return (
+    <Container
+      disableGutters
+      maxWidth="lg"
+      sx={{
+        px: {
+          xs: 2,
+          sm: 5,
+          md: 3.5,
+        },
+        my: 15,
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: {
+            xs: "column",
+            md: "row",
+          },
+          justifyContent: "center",
+          gap: 4,
+          alignItems: "center",
+        }}
+      >
+        <Box sx={{ flex: 1 }}>
+          <Typography
+            sx={{
+              fontFamily: "Poppins",
+              fontWeight: 600,
+              fontSize: "32px",
+              lineHeight: "48px",
+              color: "#161414",
+              textAlign: {
+                xs: "center",
+                md: "left",
+              },
+            }}
+          >
+            About Us
+          </Typography>
+          <Typography
+            sx={{
+              fontFamily: "Poppins",
+              fontWeight: 400,
+              fontSize: "16px",
+              lineHeight: "32px",
+              color: "#5B5B5B",
+              my: 4,
+            }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+            eu lorem et ultricies. In porta lorem at dui semper porttitor.
+            Nullam quis cursus dui. Cras tincidunt vehicula tellus eu facilisis.
+            Donec nisi turpis, iaculis et arcu a, aliquet ultrices nisl. Nam in
+            pharetra odio, ac blandit metus.
+          </Typography>
+          <SecondaryButton text="Read more" />
+        </Box>
+        <Box sx={{ flex: 1 }}>
+          <img
+            src={aboutUsImg}
+            alt=""
+            style={{
+              borderRadius: "24px",
+              maxWidth: "100%",
+            }}
+          />
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default AboutUs;
